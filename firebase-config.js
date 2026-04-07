b56d9f9dab6fdb706a6b08e9b4285a3273bf4d46
Brandon Pruitt <brapru@pm.me>
2026-04-07 19:57:39 -0400

    migrate to firebase

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCN6Bi1RZkS-98PKNu2X-0ntvp00ykJ1iA",
+  authDomain: "hundredmileclub.firebaseapp.com",
+  projectId: "hundredmileclub",
+  storageBucket: "hundredmileclub.firebasestorage.app",
+  messagingSenderId: "536683547617",
+  appId: "1:536683547617:web:5dd015fd79adda293760a0"
+};
